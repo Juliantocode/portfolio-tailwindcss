672b95f3280ff376c2fc32f476b65725e9dc30ba
maxstefan007 <maxstefan007@gmail.com>
2023-03-18 09:33:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#2dd4bf",
+        secondary: "#64748b",
+        dark: "#0f172a",
+        blue: "#1d4ed8",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
